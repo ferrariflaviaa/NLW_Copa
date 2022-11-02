f7ae4fa5be73eac7de885fed9774d17d32bab816
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-11-02 17:34:02 -0300

    feat: font installation and app change

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { NativeBaseProvider, Text, Center } from "native-base";
+import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
+import { Loading } from './src/components/Loading';
 import {THEME} from './src/styles/theme';
-
+import { Signin } from './src/screens/Signin';
 export default function App() {
+  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Center flex={1} bgColor="gray.900">
-        <Text color="white" fontSize={24}>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </Center>
+        {
+          fontsLoaded ? <Signin/> : <Loading/>
+        }
     </NativeBaseProvider>
   );
 }
