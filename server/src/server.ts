86b0fd8c81616e3d43575d40f57987b2905dfa36
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-11-02 10:45:43 -0300

    feat: application security mechanism

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,23 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors"
 import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient({
   //Aqui podemos vê as querys em logs:
   log: ['query'],
 })
+
+
 async function bootstrap() {
   const fastify = Fastify({
     //O fastify vai saltando os logs durante a nossa aplicação.
     logger: true,
   })
 
+  //Seguração da aplicação:
+  await fastify.register(cors, {
+    origin: true,
+  })
   //Criação de rotas:
 
   //Rota para os bolões:
@@ -21,7 +28,8 @@ async function bootstrap() {
   })
 
   //A porta onde será a aplicação:
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+  //Host para funcioanar em mobile.
 }
 
 bootstrap();
@@ -29,3 +37,6 @@ bootstrap();
 
 //A entrar na aplicação usando o comando "npm run dev", antes disso temos que adicionar a biblioteca "yarn i tsx -D".
 //Lembrando que temos que altera package.json nos sripts para "dev: tsx watch src/server.ts"
+//Para tarbalhar com diagrams de classes usando essas dependencias "yarn add prisma-erd-generator  @mermaid-js/mermaid-cli -D"
+//Para criar o diagrama usamos o comando "npx prisma generate"
+//Biblioteca "yarn add @fastify/cors' é mecanismo de segurança da nossa aplicações para gente dizer quais aplicações estão apitas a consumir nossos dados do back end.
