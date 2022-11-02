9cd763026a9d3fa1bd8fdfa17d9d631d11c54d48
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-11-02 17:12:48 -0300

    feat: styles

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
 import { NativeBaseProvider, Text, Center } from "native-base";
+import {THEME} from './src/styles/theme';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
         <Text color="white" fontSize={24}>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
       </Center>
