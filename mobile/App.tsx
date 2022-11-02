0992d0b869fbf0874b8c52e7570c79dba7c76a99
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-11-02 17:38:18 -0300

    styles: StatusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,17 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { NativeBaseProvider, Text, Center } from "native-base";
-import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from "@expo-google-fonts/roboto"
+// import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider, StatusBar } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from "@expo-google-fonts/roboto"
 import { Loading } from './src/components/Loading';
-import {THEME} from './src/styles/theme';
+import { THEME } from './src/styles/theme';
 import { Signin } from './src/screens/Signin';
 export default function App() {
-  const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_500Medium, Roboto_700Bold});
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-        {
-          fontsLoaded ? <Signin/> : <Loading/>
-        }
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {
+        fontsLoaded ? <Signin /> : <Loading />
+      }
     </NativeBaseProvider>
   );
 }
