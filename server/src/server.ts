8d75b64693121097f2a4d47113e7148c7e952a50
Flávia Ferrari <flaviaferrarit@gmail.com>
2022-11-02 09:02:09 -0300

    feat: creation of the bolos route

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,12 +6,19 @@ async function bootstrap() {
     logger: true,
   })
 
+  //Criação de rotas:
+
+    //Rota para os bolões:
+  fastify.get('/pools/count', () => {
+    return { count: 0 }
+  })
+
   //A porta onde será a aplicação:
-  await fastify.listen({port: 3333})
+  await fastify.listen({ port: 3333 })
 }
 
 bootstrap();
 
 
 //A entrar na aplicação usando o comando "npm run dev", antes disso temos que adicionar a biblioteca "yarn i tsx -D".
-//Lembrando que temos que altera package.json nos sripts para "dev: tsx src/server.ts"
+//Lembrando que temos que altera package.json nos sripts para "dev: tsx watch src/server.ts"
